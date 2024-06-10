1e0179416249480fb083cd6ef4bbe09797eed906
sandfrog <68484527+SandFrog@users.noreply.github.com>
2024-06-09 19:49:04 -0700

    Attempted to use ECS

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# cs312project2
-Does this work?
+# CS-312 Course Project Part 2
+
+# Requirements:
+- Ansible
+- Terraform
+- An AWS account
+- Money for the AWS account
+- 
+
+# How to Set Up:
+
+# Resources Used:
--- main.tf
+++ main.tf
@@ -4,20 +4,38 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+
   }
 
   required_version = ">= 1.2.0"
 }
-
+provider "docker" {}
 provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
+resource "aws_security_group" "minecraft_security_group" {
+  egress {
+    from_port        = 25565
+    to_port          = 25565
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    from_port        = 25565
+    to_port          = 25565
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "docker_image" "minecraft_docker" {
+
+  name              = "itzg/minecraft-server"
 }
